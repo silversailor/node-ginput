11b119953e810abaa91a9e1ba20abdd92619fb48
Antoine Robidoux <silversailsailor@gmail.com>
2013-07-24 23:25:08 -0400

    Utilisation de la génération de requête avec mysql

---
+++ gi-mysql.js
@@ -0,0 +1,79 @@
+var gi = require("./gi.js")
+  , mysql = require("mysql")
+  , EventEmitter = require("events").EventEmitter
+;
+
+/* 
+  Will execute an sql statement
+
+  cb possible value:
+    begin ( field{arr:array of field name, raw:map -> {field_name : field_desc,...} })
+    , row ( data, row_index, firld.raw )
+    , end ()
+    , error ( msg )
+*/
+
+gi.conf("alias_in_where",true);
+
+function select( data, cb ) {
+  var parsed = gi.gen.select( data )
+    , query = new Query( cb )
+  ;
+  query.exec( parsed.sql, parsed.data );
+}
+
+function Query( cb ) {
+  this.events = new EventEmitter();
+  gi.on( this.events, cb );
+  if( !gi.is.function(cb.error) ) {
+    this.events.on("error",function(msg){
+      console.log("ERROR:" + gi.dump(msg));
+    })
+  }
+}
+Query.prototype.on = function(){ gi.on.apply(this,[this].concat(arguments)) };
+Query.prototype.exec = function(statement, values){
+  var me = this;
+  pool.get(function(err, connection){
+    if(err) {
+      me.events.emit("error",gi.log("while getting connection. err dump: " + gi.dump(err), "ERROR"));
+      return;
+    }
+    var _fields = {};
+    connection.query( statement, values )
+    .on('fields', function(fields, index) {
+      _fields[index] = { raw : {}, count : 0, arr : map(fields,function(e){ return e.name }) };
+      each(fields,function(i,e){ _fields[index].raw[e.name] = e });
+      me.events.emit("begin",_fields[index]);
+    }).on("result",function(row, index){
+      me.events.emit("row",throw_up(row, _fields[index].arr), _fields[index].count++, _fields[index].raw );
+    }).on("end",function(){
+      me.events.emit("end");
+      connection.end();
+    }).on("error",function(err){
+      me.events.emit("error", gi.log( "while executing query, error dump:" + dump(err), "ERROR" ) );
+    });
+  })
+};
+
+var pool = { pool : null };
+pool.get = function (cb){
+  if(!pool.pool) {
+    throw gi.log("gi-mysql::pool.get -> no database connected. Call gi-mysql.connect before.","ERROR")
+  }
+  pool.pool.getConnection(cb);
+};
+pool.connect = function(host, username, password, database) {
+  if( !pool.pool ) {
+    pool.param = { host: host, user: username, password: password, database: database };
+    pool.pool = mysql.createPool( pool.param );
+  } else {
+    throw  gi.log( "Multiple pool connection", "WARNING" );
+  }
+};
+
+module.exports = {
+  connect : pool.connect
+  ,select : select
+  ,gi : gi
+}
