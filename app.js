03ec7bb8418ee5e86ac622221f81ac855e2787c7
Antoine Robidoux <silversailsailor@gmail.com>
2013-07-24 23:34:05 -0400

    Mise a part des nom d'utilisateur et mot de passe

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var sql = require("./gi-mysql.js")
   , gi = sql.gi
+  , db_info require("./db.js");
 ;
 
-sql.connect("ubuntu.local","antoine","","gi_test");
+sql.connect(db_info.host,db_info.user,db_info.password,db_info.database);
 
